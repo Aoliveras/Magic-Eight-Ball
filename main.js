0b4ed584845117b67ffec5031336ae8176664a0c
Adam Oliveras <theadamoliveras@gmail.com>
2018-08-23 12:13:30 -0700

    created variables for random number generation and questtion event listener, started working on function to grab input from the box and spit out answer

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 console.log('JS Loaded!');
+
+// submit question
+// give yes no or maybe answer
+
+function main() {
+
+
+    let questionEnter = document.querySelector('input');
+    //console.log(questionEnter);
+    let randoNum = Math.floor(Math.random() * 3);
+    console.log(randoNum);
+
+
+    function answerMe() {
+        let ansArr = ["yessir", "no way!", "maybe..."];
+        console.log(ansArr[randoNum]);
+        
+
+    }
+
+
+    questionEnter.addEventListener('keyup', function(e) {
+        if (e.which === 13) {
+            answerMe();
+        }
+    });
+    
+
+
+
+
+    
+
+
+
+}
+
+main();
